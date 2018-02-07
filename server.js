b5ef8d58233576b631aa4c28bebfd7a8ec1a7b19
Faurest <faurest.lupine@epitech.eu>
2018-02-07 11:56:20 +0100

    Move server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+let blockchain = require('./src/blockchain');
+let Transaction = require('./src/transactions');
+let block = require('./src/block');
+let jscoin = new blockchain();
--- src/server.js
+++
@@ -1,4 +0,0 @@
-let blockchain = require('./blockchain');
-let Transaction = require('./transactions');
-let block = require('./block');
-let jscoin = new blockchain();
