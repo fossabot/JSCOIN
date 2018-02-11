64f3bab08e1637cda334b63dbdc5a6c15b2a9216
Faurest <faurest.lupine@epitech.eu>
2018-02-11 10:33:29 +0100

    Uncomment code

--- src/blockchain.js
+++ src/blockchain.js
@@ -9,7 +9,7 @@ class Blockchain {
 	}
 
 	createGenesisBlock() {
-		return new Block("06/02/2018", "Genesis block", "0");
+		return new Block(Date.parse("06/02/2018"), [], "0");
 	}
 
 	getLatestBlock() {
@@ -32,10 +32,10 @@ class Blockchain {
 	}
 
 	createTransaction(transaction){
-		//if (this.getBalanceOfAddress(transaction.fromAddress) <= 0){
-		//	console.log("\x1b[31m", 'No enought coins of from address', "\x1b[0m");
-		//	return false;
-		//}
+		if (this.getBalanceOfAddress(transaction.fromAddress) <= 0){
+			console.log("\x1b[31m", 'No enought coins of from address', "\x1b[0m");
+			return false;
+		}
 		this.pendingTransactions.push(transaction);
 		console.log("Transaction success");
 	}
